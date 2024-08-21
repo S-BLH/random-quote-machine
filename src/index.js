aa08eb15e923a96dfb2d9cbc7de129b8e0383d8e
sumita <sumitabalouch@gmail.com>
2024-08-21 20:04:18 +0430

    solving the prob in js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client' for React 18+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+// Create a root.
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the app.
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 );
