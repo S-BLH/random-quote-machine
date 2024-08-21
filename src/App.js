a24e2872472fa32b928b2e84a08f984df368cd50
sumita <sumitabalouch@gmail.com>
2024-08-21 18:31:56 +0430

    solving jsx problems

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       { text: 'The purpose of our lives is to be happy.', author: 'Dalai Lama' },
       { text: 'Get busy living or get busy dying.', author: 'Stephen King' },
       { text: 'You only live once, but if you do it right, once is enough.', author: 'Mae West' },
-      { text: 'The best way to predict the future is to invent it.', author: 'Alan Kay' }
+      { text: 'The best way to predict the future is to invent it.', author: 'Alan Kay' },
     ];
 
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
@@ -26,8 +26,12 @@ function App() {
 
   return (
     <div id="quote-box" className="quote-box">
-      <div id="text" className="quote-text">{`"${quote}"`}</div>
-      <div id="author" className="quote-author">{`- ${author}`}</div>
+      <div id="text" className="quote-text">
+        {`"${quote}"`}
+      </div>
+      <div id="author" className="quote-author">
+        {`- ${author}`}
+      </div>
       <div id="button-container">
         <a
           id="tweet-quote"
@@ -36,9 +40,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i className="fab fa-twitter"></i> Tweet
+          <i className="fab fa-twitter" />
+          Tweet
         </a>
-        <button id="new-quote" className="quote-button" onClick={fetchQuote}>
+        <button
+          id="new-quote"
+          className="quote-button"
+          onClick={fetchQuote}
+          type="button" // Added type attribute
+        >
           New Quote
         </button>
       </div>
